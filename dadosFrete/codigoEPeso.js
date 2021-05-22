0aab181be3dd3c211daf3bbbad8b762fccae57cb
Luiz Gonzaga <luizgonzaga67@gmail.com>
2021-05-22 19:50:00 -0300

    Dados de CEP e valores a serem pagos conforme distancias.

---
+++ dadosFrete/codigoEPeso.js
@@ -0,0 +1,1022 @@
+let codigoePeso = [
+  {
+    "peso-maximo": "0,250",
+    "codigo-regiao": "SPC",
+    "preco": "6,27"
+  },
+  {
+    "peso-maximo": "0,300",
+    "codigo-regiao": "SPC",
+    "preco": "6,27"
+  },
+  {
+    "peso-maximo": "0,500",
+    "codigo-regiao": "SPC",
+    "preco": "6,27"
+  },
+  {
+    "peso-maximo": "0,750",
+    "codigo-regiao": "SPC",
+    "preco": "7,59"
+  },
+  {
+    "peso-maximo": "1,000",
+    "codigo-regiao": "SPC",
+    "preco": "7,59"
+  },
+  {
+    "peso-maximo": "2,000",
+    "codigo-regiao": "SPC",
+    "preco": "9,14"
+  },
+  {
+    "peso-maximo": "3,000",
+    "codigo-regiao": "SPC",
+    "preco": "9,99"
+  },
+  {
+    "peso-maximo": "4,000",
+    "codigo-regiao": "SPC",
+    "preco": "11,18"
+  },
+  {
+    "peso-maximo": "5,000",
+    "codigo-regiao": "SPC",
+    "preco": "12,33"
+  },
+  {
+    "peso-maximo": "6,000",
+    "codigo-regiao": "SPC",
+    "preco": "13,43"
+  },
+  {
+    "peso-maximo": "7,000",
+    "codigo-regiao": "SPC",
+    "preco": "14,29"
+  },
+  {
+    "peso-maximo": "8,000",
+    "codigo-regiao": "SPC",
+    "preco": "15,11"
+  },
+  {
+    "peso-maximo": "9,000",
+    "codigo-regiao": "SPC",
+    "preco": "15,59"
+  },
+  {
+    "peso-maximo": "10,000",
+    "codigo-regiao": "SPC",
+    "preco": "15,94"
+  },
+  {
+    "peso-maximo": "11,000",
+    "codigo-regiao": "SPC",
+    "preco": "17,37"
+  },
+  {
+    "peso-maximo": "12,000",
+    "codigo-regiao": "SPC",
+    "preco": "18,81"
+  },
+  {
+    "peso-maximo": "13,000",
+    "codigo-regiao": "SPC",
+    "preco": "20,24"
+  },
+  {
+    "peso-maximo": "14,000",
+    "codigo-regiao": "SPC",
+    "preco": "21,68"
+  },
+  {
+    "peso-maximo": "15,000",
+    "codigo-regiao": "SPC",
+    "preco": "23,11"
+  },
+  {
+    "peso-maximo": "16,000",
+    "codigo-regiao": "SPC",
+    "preco": "24,55"
+  },
+  {
+    "peso-maximo": "17,000",
+    "codigo-regiao": "SPC",
+    "preco": "25,98"
+  },
+  {
+    "peso-maximo": "18,000",
+    "codigo-regiao": "SPC",
+    "preco": "27,42"
+  },
+  {
+    "peso-maximo": "19,000",
+    "codigo-regiao": "SPC",
+    "preco": "28,85"
+  },
+  {
+    "peso-maximo": "20,000",
+    "codigo-regiao": "SPC",
+    "preco": "30,29"
+  },
+  {
+    "peso-maximo": "21,000",
+    "codigo-regiao": "SPC",
+    "preco": "31,72"
+  },
+  {
+    "peso-maximo": "22,000",
+    "codigo-regiao": "SPC",
+    "preco": "33,16"
+  },
+  {
+    "peso-maximo": "23,000",
+    "codigo-regiao": "SPC",
+    "preco": "34,59"
+  },
+  {
+    "peso-maximo": "24,000",
+    "codigo-regiao": "SPC",
+    "preco": "36,03"
+  },
+  {
+    "peso-maximo": "25,000",
+    "codigo-regiao": "SPC",
+    "preco": "37,46"
+  },
+  {
+    "peso-maximo": "26,000",
+    "codigo-regiao": "SPC",
+    "preco": "38,90"
+  },
+  {
+    "peso-maximo": "27,000",
+    "codigo-regiao": "SPC",
+    "preco": "40,33"
+  },
+  {
+    "peso-maximo": "28,000",
+    "codigo-regiao": "SPC",
+    "preco": "41,77"
+  },
+  {
+    "peso-maximo": "29,000",
+    "codigo-regiao": "SPC",
+    "preco": "43,20"
+  },
+  {
+    "peso-maximo": "30,000",
+    "codigo-regiao": "SPC",
+    "preco": "44,64"
+  },
+  {
+    "peso-maximo": "0,250",
+    "codigo-regiao": "SPG",
+    "preco": "6,27"
+  },
+  {
+    "peso-maximo": "0,300",
+    "codigo-regiao": "SPG",
+    "preco": "6,27"
+  },
+  {
+    "peso-maximo": "0,500",
+    "codigo-regiao": "SPG",
+    "preco": "6,27"
+  },
+  {
+    "peso-maximo": "0,750",
+    "codigo-regiao": "SPG",
+    "preco": "7,59"
+  },
+  {
+    "peso-maximo": "1,000",
+    "codigo-regiao": "SPG",
+    "preco": "7,59"
+  },
+  {
+    "peso-maximo": "2,000",
+    "codigo-regiao": "SPG",
+    "preco": "9,14"
+  },
+  {
+    "peso-maximo": "3,000",
+    "codigo-regiao": "SPG",
+    "preco": "9,99"
+  },
+  {
+    "peso-maximo": "4,000",
+    "codigo-regiao": "SPG",
+    "preco": "11,18"
+  },
+  {
+    "peso-maximo": "5,000",
+    "codigo-regiao": "SPG",
+    "preco": "12,33"
+  },
+  {
+    "peso-maximo": "6,000",
+    "codigo-regiao": "SPG",
+    "preco": "13,43"
+  },
+  {
+    "peso-maximo": "7,000",
+    "codigo-regiao": "SPG",
+    "preco": "14,29"
+  },
+  {
+    "peso-maximo": "8,000",
+    "codigo-regiao": "SPG",
+    "preco": "15,11"
+  },
+  {
+    "peso-maximo": "9,000",
+    "codigo-regiao": "SPG",
+    "preco": "15,59"
+  },
+  {
+    "peso-maximo": "10,000",
+    "codigo-regiao": "SPG",
+    "preco": "15,94"
+  },
+  {
+    "peso-maximo": "11,000",
+    "codigo-regiao": "SPG",
+    "preco": "17,37"
+  },
+  {
+    "peso-maximo": "12,000",
+    "codigo-regiao": "SPG",
+    "preco": "18,81"
+  },
+  {
+    "peso-maximo": "13,000",
+    "codigo-regiao": "SPG",
+    "preco": "20,24"
+  },
+  {
+    "peso-maximo": "14,000",
+    "codigo-regiao": "SPG",
+    "preco": "21,68"
+  },
+  {
+    "peso-maximo": "15,000",
+    "codigo-regiao": "SPG",
+    "preco": "23,11"
+  },
+  {
+    "peso-maximo": "16,000",
+    "codigo-regiao": "SPG",
+    "preco": "24,55"
+  },
+  {
+    "peso-maximo": "17,000",
+    "codigo-regiao": "SPG",
+    "preco": "25,98"
+  },
+  {
+    "peso-maximo": "18,000",
+    "codigo-regiao": "SPG",
+    "preco": "27,42"
+  },
+  {
+    "peso-maximo": "19,000",
+    "codigo-regiao": "SPG",
+    "preco": "28,85"
+  },
+  {
+    "peso-maximo": "20,000",
+    "codigo-regiao": "SPG",
+    "preco": "30,29"
+  },
+  {
+    "peso-maximo": "21,000",
+    "codigo-regiao": "SPG",
+    "preco": "31,72"
+  },
+  {
+    "peso-maximo": "22,000",
+    "codigo-regiao": "SPG",
+    "preco": "33,16"
+  },
+  {
+    "peso-maximo": "23,000",
+    "codigo-regiao": "SPG",
+    "preco": "34,59"
+  },
+  {
+    "peso-maximo": "24,000",
+    "codigo-regiao": "SPG",
+    "preco": "36,03"
+  },
+  {
+    "peso-maximo": "25,000",
+    "codigo-regiao": "SPG",
+    "preco": "37,46"
+  },
+  {
+    "peso-maximo": "26,000",
+    "codigo-regiao": "SPG",
+    "preco": "38,90"
+  },
+  {
+    "peso-maximo": "27,000",
+    "codigo-regiao": "SPG",
+    "preco": "40,33"
+  },
+  {
+    "peso-maximo": "28,000",
+    "codigo-regiao": "SPG",
+    "preco": "41,77"
+  },
+  {
+    "peso-maximo": "29,000",
+    "codigo-regiao": "SPG",
+    "preco": "43,20"
+  },
+  {
+    "peso-maximo": "30,000",
+    "codigo-regiao": "SPG",
+    "preco": "44,64"
+  },
+  {
+    "peso-maximo": "0,250",
+    "codigo-regiao": "SP1",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,300",
+    "codigo-regiao": "SP1",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,500",
+    "codigo-regiao": "SP1",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,750",
+    "codigo-regiao": "SP1",
+    "preco": "10,54"
+  },
+  {
+    "peso-maximo": "1,000",
+    "codigo-regiao": "SP1",
+    "preco": "10,54"
+  },
+  {
+    "peso-maximo": "2,000",
+    "codigo-regiao": "SP1",
+    "preco": "12,67"
+  },
+  {
+    "peso-maximo": "3,000",
+    "codigo-regiao": "SP1",
+    "preco": "13,88"
+  },
+  {
+    "peso-maximo": "4,000",
+    "codigo-regiao": "SP1",
+    "preco": "14,81"
+  },
+  {
+    "peso-maximo": "5,000",
+    "codigo-regiao": "SP1",
+    "preco": "15,84"
+  },
+  {
+    "peso-maximo": "6,000",
+    "codigo-regiao": "SP1",
+    "preco": "16,70"
+  },
+  {
+    "peso-maximo": "7,000",
+    "codigo-regiao": "SP1",
+    "preco": "17,65"
+  },
+  {
+    "peso-maximo": "8,000",
+    "codigo-regiao": "SP1",
+    "preco": "18,55"
+  },
+  {
+    "peso-maximo": "9,000",
+    "codigo-regiao": "SP1",
+    "preco": "19,09"
+  },
+  {
+    "peso-maximo": "10,000",
+    "codigo-regiao": "SP1",
+    "preco": "19,48"
+  },
+  {
+    "peso-maximo": "11,000",
+    "codigo-regiao": "SP1",
+    "preco": "21,21"
+  },
+  {
+    "peso-maximo": "12,000",
+    "codigo-regiao": "SP1",
+    "preco": "22,97"
+  },
+  {
+    "peso-maximo": "13,000",
+    "codigo-regiao": "SP1",
+    "preco": "24,73"
+  },
+  {
+    "peso-maximo": "14,000",
+    "codigo-regiao": "SP1",
+    "preco": "26,47"
+  },
+  {
+    "peso-maximo": "15,000",
+    "codigo-regiao": "SP1",
+    "preco": "28,23"
+  },
+  {
+    "peso-maximo": "16,000",
+    "codigo-regiao": "SP1",
+    "preco": "29,98"
+  },
+  {
+    "peso-maximo": "17,000",
+    "codigo-regiao": "SP1",
+    "preco": "31,73"
+  },
+  {
+    "peso-maximo": "18,000",
+    "codigo-regiao": "SP1",
+    "preco": "33,48"
+  },
+  {
+    "peso-maximo": "19,000",
+    "codigo-regiao": "SP1",
+    "preco": "35,24"
+  },
+  {
+    "peso-maximo": "20,000",
+    "codigo-regiao": "SP1",
+    "preco": "36,99"
+  },
+  {
+    "peso-maximo": "21,000",
+    "codigo-regiao": "SP1",
+    "preco": "38,75"
+  },
+  {
+    "peso-maximo": "22,000",
+    "codigo-regiao": "SP1",
+    "preco": "40,49"
+  },
+  {
+    "peso-maximo": "23,000",
+    "codigo-regiao": "SP1",
+    "preco": "42,24"
+  },
+  {
+    "peso-maximo": "24,000",
+    "codigo-regiao": "SP1",
+    "preco": "44,00"
+  },
+  {
+    "peso-maximo": "25,000",
+    "codigo-regiao": "SP1",
+    "preco": "45,76"
+  },
+  {
+    "peso-maximo": "26,000",
+    "codigo-regiao": "SP1",
+    "preco": "47,50"
+  },
+  {
+    "peso-maximo": "27,000",
+    "codigo-regiao": "SP1",
+    "preco": "49,26"
+  },
+  {
+    "peso-maximo": "28,000",
+    "codigo-regiao": "SP1",
+    "preco": "51,01"
+  },
+  {
+    "peso-maximo": "29,000",
+    "codigo-regiao": "SP1",
+    "preco": "52,75"
+  },
+  {
+    "peso-maximo": "30,000",
+    "codigo-regiao": "SP1",
+    "preco": "54,51"
+  },
+  {
+    "peso-maximo": "0,250",
+    "codigo-regiao": "SP2",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,300",
+    "codigo-regiao": "SP2",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,500",
+    "codigo-regiao": "SP2",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,750",
+    "codigo-regiao": "SP2",
+    "preco": "10,54"
+  },
+  {
+    "peso-maximo": "1,000",
+    "codigo-regiao": "SP2",
+    "preco": "10,54"
+  },
+  {
+    "peso-maximo": "2,000",
+    "codigo-regiao": "SP2",
+    "preco": "12,67"
+  },
+  {
+    "peso-maximo": "3,000",
+    "codigo-regiao": "SP2",
+    "preco": "13,88"
+  },
+  {
+    "peso-maximo": "4,000",
+    "codigo-regiao": "SP2",
+    "preco": "14,81"
+  },
+  {
+    "peso-maximo": "5,000",
+    "codigo-regiao": "SP2",
+    "preco": "15,84"
+  },
+  {
+    "peso-maximo": "6,000",
+    "codigo-regiao": "SP2",
+    "preco": "16,70"
+  },
+  {
+    "peso-maximo": "7,000",
+    "codigo-regiao": "SP2",
+    "preco": "17,65"
+  },
+  {
+    "peso-maximo": "8,000",
+    "codigo-regiao": "SP2",
+    "preco": "18,55"
+  },
+  {
+    "peso-maximo": "9,000",
+    "codigo-regiao": "SP2",
+    "preco": "19,09"
+  },
+  {
+    "peso-maximo": "10,000",
+    "codigo-regiao": "SP2",
+    "preco": "19,48"
+  },
+  {
+    "peso-maximo": "11,000",
+    "codigo-regiao": "SP2",
+    "preco": "21,21"
+  },
+  {
+    "peso-maximo": "12,000",
+    "codigo-regiao": "SP2",
+    "preco": "22,97"
+  },
+  {
+    "peso-maximo": "13,000",
+    "codigo-regiao": "SP2",
+    "preco": "24,73"
+  },
+  {
+    "peso-maximo": "14,000",
+    "codigo-regiao": "SP2",
+    "preco": "26,47"
+  },
+  {
+    "peso-maximo": "15,000",
+    "codigo-regiao": "SP2",
+    "preco": "28,23"
+  },
+  {
+    "peso-maximo": "16,000",
+    "codigo-regiao": "SP2",
+    "preco": "29,98"
+  },
+  {
+    "peso-maximo": "17,000",
+    "codigo-regiao": "SP2",
+    "preco": "31,73"
+  },
+  {
+    "peso-maximo": "18,000",
+    "codigo-regiao": "SP2",
+    "preco": "33,48"
+  },
+  {
+    "peso-maximo": "19,000",
+    "codigo-regiao": "SP2",
+    "preco": "35,24"
+  },
+  {
+    "peso-maximo": "20,000",
+    "codigo-regiao": "SP2",
+    "preco": "36,99"
+  },
+  {
+    "peso-maximo": "21,000",
+    "codigo-regiao": "SP2",
+    "preco": "38,75"
+  },
+  {
+    "peso-maximo": "22,000",
+    "codigo-regiao": "SP2",
+    "preco": "40,49"
+  },
+  {
+    "peso-maximo": "23,000",
+    "codigo-regiao": "SP2",
+    "preco": "42,24"
+  },
+  {
+    "peso-maximo": "24,000",
+    "codigo-regiao": "SP2",
+    "preco": "44,00"
+  },
+  {
+    "peso-maximo": "25,000",
+    "codigo-regiao": "SP2",
+    "preco": "45,76"
+  },
+  {
+    "peso-maximo": "26,000",
+    "codigo-regiao": "SP2",
+    "preco": "47,50"
+  },
+  {
+    "peso-maximo": "27,000",
+    "codigo-regiao": "SP2",
+    "preco": "49,26"
+  },
+  {
+    "peso-maximo": "28,000",
+    "codigo-regiao": "SP2",
+    "preco": "51,01"
+  },
+  {
+    "peso-maximo": "29,000",
+    "codigo-regiao": "SP2",
+    "preco": "52,75"
+  },
+  {
+    "peso-maximo": "30,000",
+    "codigo-regiao": "SP2",
+    "preco": "54,51"
+  },
+  {
+    "peso-maximo": "0,250",
+    "codigo-regiao": "SP3",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,300",
+    "codigo-regiao": "SP3",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,500",
+    "codigo-regiao": "SP3",
+    "preco": "7,80"
+  },
+  {
+    "peso-maximo": "0,750",
+    "codigo-regiao": "SP3",
+    "preco": "10,54"
+  },
+  {
+    "peso-maximo": "1,000",
+    "codigo-regiao": "SP3",
+    "preco": "10,54"
+  },
+  {
+    "peso-maximo": "2,000",
+    "codigo-regiao": "SP3",
+    "preco": "12,67"
+  },
+  {
+    "peso-maximo": "3,000",
+    "codigo-regiao": "SP3",
+    "preco": "13,88"
+  },
+  {
+    "peso-maximo": "4,000",
+    "codigo-regiao": "SP3",
+    "preco": "14,81"
+  },
+  {
+    "peso-maximo": "5,000",
+    "codigo-regiao": "SP3",
+    "preco": "15,84"
+  },
+  {
+    "peso-maximo": "6,000",
+    "codigo-regiao": "SP3",
+    "preco": "16,70"
+  },
+  {
+    "peso-maximo": "7,000",
+    "codigo-regiao": "SP3",
+    "preco": "17,65"
+  },
+  {
+    "peso-maximo": "8,000",
+    "codigo-regiao": "SP3",
+    "preco": "18,55"
+  },
+  {
+    "peso-maximo": "9,000",
+    "codigo-regiao": "SP3",
+    "preco": "19,09"
+  },
+  {
+    "peso-maximo": "10,000",
+    "codigo-regiao": "SP3",
+    "preco": "19,48"
+  },
+  {
+    "peso-maximo": "11,000",
+    "codigo-regiao": "SP3",
+    "preco": "21,21"
+  },
+  {
+    "peso-maximo": "12,000",
+    "codigo-regiao": "SP3",
+    "preco": "22,97"
+  },
+  {
+    "peso-maximo": "13,000",
+    "codigo-regiao": "SP3",
+    "preco": "24,73"
+  },
+  {
+    "peso-maximo": "14,000",
+    "codigo-regiao": "SP3",
+    "preco": "26,47"
+  },
+  {
+    "peso-maximo": "15,000",
+    "codigo-regiao": "SP3",
+    "preco": "28,23"
+  },
+  {
+    "peso-maximo": "16,000",
+    "codigo-regiao": "SP3",
+    "preco": "29,98"
+  },
+  {
+    "peso-maximo": "17,000",
+    "codigo-regiao": "SP3",
+    "preco": "31,73"
+  },
+  {
+    "peso-maximo": "18,000",
+    "codigo-regiao": "SP3",
+    "preco": "33,48"
+  },
+  {
+    "peso-maximo": "19,000",
+    "codigo-regiao": "SP3",
+    "preco": "35,24"
+  },
+  {
+    "peso-maximo": "20,000",
+    "codigo-regiao": "SP3",
+    "preco": "36,99"
+  },
+  {
+    "peso-maximo": "21,000",
+    "codigo-regiao": "SP3",
+    "preco": "38,75"
+  },
+  {
+    "peso-maximo": "22,000",
+    "codigo-regiao": "SP3",
+    "preco": "40,49"
+  },
+  {
+    "peso-maximo": "23,000",
+    "codigo-regiao": "SP3",
+    "preco": "42,24"
+  },
+  {
+    "peso-maximo": "24,000",
+    "codigo-regiao": "SP3",
+    "preco": "44,00"
+  },
+  {
+    "peso-maximo": "25,000",
+    "codigo-regiao": "SP3",
+    "preco": "45,76"
+  },
+  {
+    "peso-maximo": "26,000",
+    "codigo-regiao": "SP3",
+    "preco": "47,50"
+  },
+  {
+    "peso-maximo": "27,000",
+    "codigo-regiao": "SP3",
+    "preco": "49,26"
+  },
+  {
+    "peso-maximo": "28,000",
+    "codigo-regiao": "SP3",
+    "preco": "51,01"
+  },
+  {
+    "peso-maximo": "29,000",
+    "codigo-regiao": "SP3",
+    "preco": "52,75"
+  },
+  {
+    "peso-maximo": "30,000",
+    "codigo-regiao": "SP3",
+    "preco": "54,51"
+  },
+  {
+    "peso-maximo": "0,250",
+    "codigo-regiao": "SPB",
+    "preco": "18,88"
+  },
+  {
+    "peso-maximo": "0,300",
+    "codigo-regiao": "SPB",
+    "preco": "18,88"
+  },
+  {
+    "peso-maximo": "0,500",
+    "codigo-regiao": "SPB",
+    "preco": "19,57"
+  },
+  {
+    "peso-maximo": "0,750",
+    "codigo-regiao": "SPB",
+    "preco": "21,82"
+  },
+  {
+    "peso-maximo": "1,000",
+    "codigo-regiao": "SPB",
+    "preco": "21,82"
+  },
+  {
+    "peso-maximo": "2,000",
+    "codigo-regiao": "SPB",
+    "preco": "24,07"
+  },
+  {
+    "peso-maximo": "3,000",
+    "codigo-regiao": "SPB",
+    "preco": "26,32"
+  },
+  {
+    "peso-maximo": "4,000",
+    "codigo-regiao": "SPB",
+    "preco": "28,95"
+  },
+  {
+    "peso-maximo": "5,000",
+    "codigo-regiao": "SPB",
+    "preco": "31,21"
+  },
+  {
+    "peso-maximo": "6,000",
+    "codigo-regiao": "SPB",
+    "preco": "33,71"
+  },
+  {
+    "peso-maximo": "7,000",
+    "codigo-regiao": "SPB",
+    "preco": "36,10"
+  },
+  {
+    "peso-maximo": "8,000",
+    "codigo-regiao": "SPB",
+    "preco": "38,62"
+  },
+  {
+    "peso-maximo": "9,000",
+    "codigo-regiao": "SPB",
+    "preco": "41,15"
+  },
+  {
+    "peso-maximo": "10,000",
+    "codigo-regiao": "SPB",
+    "preco": "43,90"
+  },
+  {
+    "peso-maximo": "11,000",
+    "codigo-regiao": "SPB",
+    "preco": "49,34"
+  },
+  {
+    "peso-maximo": "12,000",
+    "codigo-regiao": "SPB",
+    "preco": "54,77"
+  },
+  {
+    "peso-maximo": "13,000",
+    "codigo-regiao": "SPB",
+    "preco": "60,21"
+  },
+  {
+    "peso-maximo": "14,000",
+    "codigo-regiao": "SPB",
+    "preco": "65,65"
+  },
+  {
+    "peso-maximo": "15,000",
+    "codigo-regiao": "SPB",
+    "preco": "71,08"
+  },
+  {
+    "peso-maximo": "16,000",
+    "codigo-regiao": "SPB",
+    "preco": "76,52"
+  },
+  {
+    "peso-maximo": "17,000",
+    "codigo-regiao": "SPB",
+    "preco": "81,95"
+  },
+  {
+    "peso-maximo": "18,000",
+    "codigo-regiao": "SPB",
+    "preco": "87,39"
+  },
+  {
+    "peso-maximo": "19,000",
+    "codigo-regiao": "SPB",
+    "preco": "92,83"
+  },
+  {
+    "peso-maximo": "20,000",
+    "codigo-regiao": "SPB",
+    "preco": "98,26"
+  },
+  {
+    "peso-maximo": "21,000",
+    "codigo-regiao": "SPB",
+    "preco": "103,70"
+  },
+  {
+    "peso-maximo": "22,000",
+    "codigo-regiao": "SPB",
+    "preco": "109,13"
+  },
+  {
+    "peso-maximo": "23,000",
+    "codigo-regiao": "SPB",
+    "preco": "114,57"
+  },
+  {
+    "peso-maximo": "24,000",
+    "codigo-regiao": "SPB",
+    "preco": "120,00"
+  },
+  {
+    "peso-maximo": "25,000",
+    "codigo-regiao": "SPB",
+    "preco": "125,44"
+  },
+  {
+    "peso-maximo": "26,000",
+    "codigo-regiao": "SPB",
+    "preco": "130,88"
+  },
+  {
+    "peso-maximo": "27,000",
+    "codigo-regiao": "SPB",
+    "preco": "136,31"
+  },
+  {
+    "peso-maximo": "28,000",
+    "codigo-regiao": "SPB",
+    "preco": "141,75"
+  },
+  {
+    "peso-maximo": "29,000",
+    "codigo-regiao": "SPB",
+    "preco": "147,18"
+  },
+  {
+    "peso-maximo": "30,000",
+    "codigo-regiao": "SPB",
+    "preco": "152,62"
+  }
+]
